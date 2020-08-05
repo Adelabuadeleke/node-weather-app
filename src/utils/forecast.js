1254398d9ef592de4f58225a3d3a6f1ba3796796
Adeleke <kamade123@gmail.com>
2020-08-04 23:01:29 -0700

    new feature added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ request({url, json:true}, (error, {body}) =>{
   } else if(body.error) {
     callback('Unable to find location', undefined)
   } else {
-    callback(undefined, body.current.weather_descriptions[0] + '. it is currently ' + body.current.temperature + 'degrees out ,it feels like ' + body.current.feelslike + 'degrees out.')
+    callback(undefined, body.current.weather_descriptions[0] + '. it is currently ' + body.current.temperature + 'degrees out ,it feels like ' + body.current.feelslike + 'degrees out, and the humidity is ' + body.current.humidity + '%')
   }
 
 })
